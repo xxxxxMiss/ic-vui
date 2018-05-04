8b7446318eb1bc840443663b423d08b763c81ab3
chenxianlong <794465731@qq.com>
2018-05-04 13:41:40 +0800

    improve: update iconfont

--- packages/index.js
+++ packages/index.js
@@ -32,7 +32,7 @@ import IcTimeline from './timeline'
 import IcToast from './toast'
 import IcUpload from './upload'
 
-const version = '0.0.9472'
+const version = '0.0.9476'
 const components = [
   IcAvatar,
   IcBadge,
